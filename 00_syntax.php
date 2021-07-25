14b5703d95a0cb182b1cc713c85eb131cf16475d
Todd Spainhour <toddspainhour@gmail.com>
2021-07-24 21:51:33 -0500

    syntax, comments, and variables sections complete

---
+++ 00_syntax.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+<!--Echo HTML tags-->
+<?php
+ echo "Hello World. I'm PHP.";
+ echo " And here's another line of PHP inside same tag."
+?>
+
+<?php
+  echo '<br>';
+  echo "And I'm from a different PHP tag";
+  echo '<br>';
+  echo '<button>A useless button</button>';
+?>
+
+</body>
+</html>
