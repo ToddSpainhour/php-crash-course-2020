2c5861814a8ba5762053fb99479a54e319e1a74a
Todd Spainhour <toddspainhour@gmail.com>
2021-07-25 11:00:43 -0500

    numbers, strings, and arrays sections complete

--- 04_strings.php
+++ 04_strings.php
@@ -1,13 +1,65 @@
 <?php
 
 // Create simple string
+$name = 'Todd';
+$singleQuoteString = 'Hello. My name is $name.';
+$doubleQuoteString = "Hello. My name is $name."; // like JavaScript's template literal
+echo $singleQuoteString.'<br>';
+echo $doubleQuoteString.'<br>';
+
+
 
 // String concatenation
+echo 'Hello '.' world '.' and PHP'.'<br>';
+
+
 
 // String functions
+$exampleString = "     Hello World     "; // has extra spaces
+
+echo  $exampleString . '<br>';
+echo " strlen: " . strlen($exampleString) . '<br>'; // length of string
+echo " trim: " . trim($exampleString) . '<br>'; // removes whitespace from beginning and end of string
+echo " ltrim: " . ltrim($exampleString) . '<br>'; // removes whitespace from the left side of the string
+echo " rtrim: " . rtrim($exampleString) . '<br>'; // removes whitespace from the right side of the string
+echo " str_word_count: " . str_word_count($exampleString) . '<br>'; // number of words in string
+echo " strrev: " . strrev($exampleString) . '<br>'; // reverses string
+echo " strtoupper: " . strtoupper($exampleString) . '<br>'; // all characters to uppercase
+echo " strtolower: " . strtolower($exampleString) . '<br>'; // all characters to lowercase
+echo " ucfirst: " . ucfirst('hello') . '<br>'; // change first character to uppercase
+echo " lcfirst: " . lcfirst('HELLO') . '<br>'; // change first character to lowercase
+echo " ucwords: " . ucwords('hello world') . '<br>'; // changes first character of words to uppercase
+echo " strpos: " . strpos($exampleString, 'world') . '<br>'; // finds position of selected word;  case sensitive; includes whitespace;
+echo " stripos: " . stripos($exampleString, 'world') . '<br>'; // finds position of selected word;  case insensitive; includes whitespace;
+echo " substr: " . substr($exampleString, 8) . '<br>'; // returns substring starting at selected position; optional 3rd argument to say how many characters to grab
+echo " str_replace: " . str_replace('world', 'PHP', $exampleString) . '<br>'; // case sensitive
+echo " str_ireplace: " . str_ireplace('world', 'PHP', $exampleString) . '<br>'; // case insensitive
+
+
 
 // Multiline text and line breaks
+$longText = "
+Hello, my name is Todd. 
+This is the PHP Crash Course,
+and so far so good.
+";
+
+echo $longText . '<br>'; // displays all on same line in browser
+echo nl2br($longText) . '<br>'; // adds <br> tags whenever it finds a new line
+
 
 // Multiline text and reserve html tags
+$longText = "
+Hello, my name is <b>Todd</b>. 
+This is the <b>PHP Crash Course</b>,
+and so far so good.
+";
+
+echo "just echoed: " . $longText . '<br>'; // displays all on same line in browser
+echo "nl2br: " . nl2br($longText) . '<br>'; // follows formatting in original string
+echo "htmlentities: " . htmlentities($longText) . '<br>'; // displays html tags in string
+echo "nl&br and htmlentities: " . nl2br(htmlentities($longText)) . '<br>'; // displays html tags and keeps formatting
+
+
 
 // https://www.php.net/manual/en/ref.strings.php
--- 05_arrays.php
+++ 05_arrays.php
@@ -1,61 +1,233 @@
 <?php
 
 // Create array
+$fruits = ["Banana", "Apple", "Orange"]; // single or double quotes ok; for a variable use double quotes
+// $fruits = array(); is the old way to declare an array 
+
+
 
 // Print the whole array
+var_dump($fruits);
+// to make it more readable wrap the dump with <pre> tags
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
+
 
 // Get element by index
+echo $fruits[1] . '<br>';
+
+
 
 // Set element by index
+$fruits[0] = 'Peach';
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
+
 
 // Check if array has element at index 2
+isset($fruits[2]); // true; there is an item at index position 2; if not... false
+
+
 
 // Append element
+$fruits[] = 'Banana'; // like JavaScript's .push
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
+
 
 // Print the length of the array
+echo count($fruits) . '<br>';
+
+
 
 // Add element at the end of the array
+array_push($fruits, "Grape"); // first argument is the array; second is the value to add
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
+
 
 // Remove element from the end of the array
+echo array_pop($fruits);
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
+
 
 // Add element at the beginning of the array
+array_unshift($fruits, 'Cherry');
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
+
 
 // Remove element from the beginning of the array
+array_shift($fruits);
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
+
 
 // Split the string into an array
+$string = "newBanana,newApple,newPeach";
+echo '<pre>';
+var_dump(explode(",", $string));
+echo '</pre>';
+
+
 
 // Combine array elements into string
+echo implode("&", $fruits);
+
+
 
 // Check if element exist in the array
+echo '<pre>';
+var_dump(in_array("Apple", $fruits));// true
+var_dump(in_array("Mango", $fruits));// fasle
+echo '</pre>';
+
+
 
 // Search element index in the array
+echo '<pre>';
+var_dump(array_search('Mango', $fruits)); // false
+var_dump(array_search('Apple', $fruits)); // returns index position if true
+echo '</pre>';
+
+
 
 // Merge two arrays
+$vegetables = ["Potato", "Cucumber"];
 
-// Sorting of array (Reverse order also)
+$fruitsAndVeggiesArray = array_merge($fruits, $vegetables); // combined in order of arguments
+echo '<pre>';
+var_dump($fruitsAndVeggiesArray);
+echo '</pre>';
+
+
+
+ // spread operator to combine arrays
+$fruitsAndVeggiesArrayWithSpreadOperator = ([...$fruits, ...$vegetables]); // combined in order of arguments
+echo '<pre>';
+var_dump($fruitsAndVeggiesArrayWithSpreadOperator);
+echo '</pre>';
+
+
+
+// Sorting of array
+sort($fruits);
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
+
+// Reverse sorting of array
+rsort($fruits);
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
 
 
 // https://www.php.net/manual/en/ref.array.php
+
+
 
 // ============================================
 // Associative arrays
 // ============================================
 
+
+
 // Create an associative array
+$person = [
+  'name' => 'Todd',
+  'surname' => 'Spainhour',
+  'age' => 40,
+  'hobbies' => ['hiking', 'video games']
+];
+echo '<pre>';
+print_r($person); // print_r formats the output in a more readable way
+echo '</pre>';
+
+
 
 // Get element by key
+echo $person['name'].'<br>'; // returns value for name key
+echo $person['address'].'<br>'; // if key doesn't exist it returns error saying Undefined Index;
+
+
 
 // Set element by key
+$person['current activity'] = 'Learning PHP'; // create new key value pair
+echo '<pre>';
+print_r($person); 
+echo '</pre>';
+
+
 
 // Null coalescing assignment operator. Since PHP 7.4
+// if(!isset($person['address'])) {
+//   $person['address'] = 'unknown';
+// } // if this key doesn't exist, create it and set it specified value
+// echo '<pre>';
+// print_r($person); 
+// echo '</pre>';
 
-// Check if array has specific key
+// here's the PHP 7.4 way to do the same thing
+$person['address'] ??= 'unknown'; // ??= is this value set?
+echo '<pre>';
+print_r($person); 
+echo '</pre>';
+
+
 
 // Print the keys of the array
+echo '<pre>';
+var_dump(array_keys($person));
+echo '<pre>';
+
+
 
 // Print the values of the array
+echo '<pre>';
+var_dump(array_values($person));
+echo '<pre>';
 
-// Sorting associative arrays by values, by keys
+
+
+// Sorting associative arrays by keys
+ksort($person);
+echo '<pre>';
+var_dump($person);
+echo '<pre>';
+
+
+
+// Sorting associative arrays by values
+asort($person); 
+echo '<pre>';
+var_dump($person);
+echo '<pre>';
+
 
 
 // Two dimensional arrays
+$todos = [
+  ['title' => 'title 1', 'completed' => true],
+  ['title2' => 'title 2', 'completed' => false]
+];
+echo '<pre>';
+var_dump($todos);
+echo '<pre>';
