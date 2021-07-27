4552714de1dbb9e7d2a792b4a6e4ed6485e583eb
Todd Spainhour <toddspainhour@gmail.com>
2021-07-26 21:40:48 -0500

    first half of OOP complete

--- 12_oop/index.php
+++ 12_oop/index.php
@@ -1,6 +1,74 @@
 <?php
 
 // What is class and instance
+class Person {
+  public $name;
+  public $surname;
+  private $age;
+  public static $counter = 0;
+
+
+  public function __construct($name, $surname)
+  {
+    $this->name = $name;
+    $this->surname = $surname;
+    self::$counter++;
+  }
+
+public function setAge($age) 
+{
+  $this->age = $age;
+}
+
+public function getAge()
+{
+  echo "inside getAge() method" . '<br>';
+  return $this->age;
+}
+
+public static function getCounter()
+{
+  echo "inside getCounter function. Amount in instances created: ";
+  return self::$counter;
+}
+
+}
+
+
+
+$personOne = new Person("Jimbo", "McLean");
+$personTwo = new Person("Cindi", "Lue");
+
+
+$personOne->setAge = 40;
+$personTwo->setAge = 39;
+
+
+echo $personOne->getAge();
+echo $personTwo->getAge();
+
+echo Person::getCounter();
+
+// $personOne->name = 'Jimbo'; 
+// $personOne->surname = 'McLean'; 
+
+
+// $personTwo->name = 'Cindi'; 
+// $personTwo->surname = 'Lue'; 
+
+echo '<pre>';
+var_dump($personOne);
+echo '</pre>';
+
+
+
+echo '<pre>';
+var_dump($personTwo);
+echo '</pre>';
+
+//echo $personOne->name . '<br>';
+// echo $personTwo->name . '<br>';
+
 
 // Create Person class in Person.php
 
